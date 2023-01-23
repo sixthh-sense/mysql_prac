ee64389714067f5f78b6d2de6f6209923e3e27a9
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-23 12:11:03 +0900

    개별 db url Secrets 탭으로 지정

--- src/main/java/udemy/mysql/jdbc/create/CreateAccount.java
+++ src/main/java/udemy/mysql/jdbc/create/CreateAccount.java
@@ -10,7 +10,6 @@ import java.sql.Statement;
 public class CreateAccount {
     static Secret secret = new Secret();
     public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/db_practice"; // use할 db명 말미에 명시
         Connection con = null;
         Statement stat = null;
         try {
@@ -31,7 +30,7 @@ public class CreateAccount {
         // 현재 사용하는 DB 나타내기: SELECT <database-name>();
 
         try {
-            con = DriverManager.getConnection(url, secret.db_id, secret.db_pw);
+            con = DriverManager.getConnection(secret.url_db_practice, secret.db_id, secret.db_pw);
             stat = con.createStatement();
             stat.executeUpdate(createString);
             System.out.println("The table account has been successfully created!");
--- src/main/java/udemy/mysql/jdbc/create/CreateCustomer.java
+++ src/main/java/udemy/mysql/jdbc/create/CreateCustomer.java
@@ -11,7 +11,6 @@ public class CreateCustomer {
 
     static Secret secret = new Secret();
     public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/db_practice"; // use할 db명 말미에 명시
         Connection con = null;
         Statement stat = null;
         try {
@@ -31,7 +30,7 @@ public class CreateCustomer {
         // 현재 사용하는 DB 나타내기: SELECT <database-name>();
 
         try {
-            con = DriverManager.getConnection(url, secret.db_id, secret.db_pw);
+            con = DriverManager.getConnection(secret.url_db_practice, secret.db_id, secret.db_pw);
             stat = con.createStatement();
             stat.executeUpdate(createString);
             System.out.println("The table customer has been successfully created!");
